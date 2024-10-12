b6128054af5ee86f1b5ec3ae217fa2814137ba32
gokayybaz <gokaybaz2000@outlook.com>
2024-10-12 21:40:28 +0300

    Add project files.

---
+++ 01-MainSubjects/Program.cs
@@ -0,0 +1,183 @@
+﻿// using: Kullanacağımız kodlara ait kütüphaneleri tutmaktadır.
+// Bu şekilde kütüphanelerin kullanılmasının sebebi;
+/*
+   Programlamada her bir kod bir kütüphaneye yani using değerine aittir.
+   Yazılım dünyası, neye ihtiyacın varsa onu buraya using ile dahil et veya ederim diyor.
+   Ben sana bir başlık altında sunmuyorum diyor çünkü bir başlık altında bütün kütüphaneleri sunsaydı sistem çok yorulacaktı.
+   Bu da bize Yazılım Dünyasının : Böl - Parçala - Yönet mantığına uyduğunu gösterir.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Projenin ismidir, yani kapsama alanıdır.
+namespace _01_MainSubjects
+{
+	// Program derlenince çalışacak ana class'tır.
+	internal class Program
+	{
+		// Program sınıfı derlenince çalışacak ana metottur.
+		static void Main(string[] args)
+		{   // Yazdığımız kodları bölüm bölüm ayırabilmek için #region kullanırız.
+			#region Yazdırma Komutları
+			// Program derlenince konsol ekranına: Hello World ! yazılmasını sağlamaktadır.
+			// Not: Bir yapının metot olduğunu anlamak için, intellisense ekranında sol ikonu mor bir küp ise o bir metottur.
+			// Metot içerisinde çıktıda gösterilecek içerik yazılır.
+			// İçerik eğer alfabetik bir metin ise çift tırnak içerisinde yazılır.
+
+			// Not: C# programlama dilinde, derleyicinin kodları yorumlayabilmesi için yani kodun orada bittiğini anlayabilmesi için noktalı virgül eklenmek zorunludur.
+
+			// Console.Write("Merhaba Dünya!");
+
+			// WriteLine ise içerisindeki içeriği konsola yazar sonrasında bir satır boşluk bırakır.
+			// Console.WriteLine("Selam");
+
+			// Enter tuşuna basana kadar ekranı bekletecek olan koddur.
+			// Console.Read();
+
+			/*
+            Console.WriteLine("***** Yemek Kategorileri *****");
+            Console.WriteLine();
+            Console.WriteLine("1-Çorbalar");
+            Console.WriteLine("2-Ana Yemekler");
+            Console.WriteLine("3-Soğuk Başlangıçlar");
+            Console.WriteLine("4-Salatalar");
+            Console.WriteLine("5-Tatlılar");
+            Console.WriteLine("6-İçecekler");
+			Console.WriteLine("***** Yemek Kategorileri *****");
+            */
+			// Bölümün tamamlandığı yere #endregion koymamız gerekmektedir.
+			#endregion
+
+			#region String Değişkenler
+			// Çeşitli türe sahip verileri bellekte tutabilmek ve sonradan işlem yapabilmek için değişkenleri kullanırız.
+			// Değişkenleri kullanmamızın bir diğer sebebi de bir veriyi birden fazla yerde kullanmak ve kullandığımız veriyi tek bir
+			// noktadan değiştirebilmektir.
+
+			// string: Alfabetik yani metinsel türde veri alabilen bir değişken türüdür.
+			// string variableName;
+			// Değişken tanımlama adımları: degiskenturu degiskenAdi;
+			string name;
+			// Tanımladığımız değişkene ilgili değişken türüne göre bir değer ataması gerçekleştiriyoruz;
+			// "=" programlamada atama operatörü olarak kullanılır.
+			name = "Gökay Baz";
+            // Değişkene tanımlandıktan ve içerisine değer atandıktan sonra ilgili değişken değeri ile beraber bellekte kendine bir alan ayırır.
+            // Oluşturduğumuz değişkeni konsola yazdıralım;
+
+			// Bundan önce konsol ekranına yazdırırken çift tırnak içerisine veri girerek yazdırıyorduk, ama artık değişken kullandığımız için-
+			// değişkenimizin ismini yazıyoruz. İlgili değişken ismi bize yazdıracağımız değeri temsil ettiği için bu şekilde kullanıyoruz.
+			// Böylece her bu değeri kullanacığımız yerde değişken adını kullanırsak, sonradan değişkenin değerini tanımladğımız yerde değiştirerek
+			// bütün bu değişkeni kullandığımız alanlarda ilgili değerin güncellenmesini sağlarız, böylece daha merkezi bir yönetim sağlanır.
+            //Console.WriteLine(name);
+
+			// C# da değişken tanımlarken, değişken ismini isimlendirme kuralı olan: camelCase ile oluştururuz.
+			// Değişken ismi olabildiğince açık ve yalın olmalıdır.
+			string customerName;
+			string customerSurname;
+			string customerPhone;
+			// Tek satırda aynı türden birden fazla değişken tanımlaması yapabilmekteyiz.
+			string customerEmail, customerDistrict, customerCity;
+
+			customerName = "Hakkı";
+			customerSurname = "Baz";
+			customerPhone = "+905512331163";
+			customerEmail = "hbaz456@gmail.com";
+			customerDistrict = "Kozan";
+			customerCity = "Adana";
+
+            //Console.WriteLine("**** Rezervasyon Kartları ****");
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------------------------------");
+			// String Toplama: Birden fazla string değeri "+" yani toplama operatörü ile toplayıp yan yana yazılmasını sağlayabiliriz.
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+			// "\n" string ifadelerde satır atlama için kullanılır.
+            //Console.WriteLine("İletişim: " + customerPhone + "\n" + "Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + customerCity + "/" + customerCity);
+			//Console.WriteLine("------------------------------------------------------");
+
+			//Console.WriteLine("");
+
+			// Programlamada hiyerarşik bir atama sıralandırması vardır, yukarıdan başlar satır satır okuyarak son satıra kadar okuma işlemlerini yapar.
+			// Console.WriteLine("Eski Değer: " + customerName);
+			// Biz burada önceden tanımladığımız değeri değiştiriyoruz.
+			// customerName = "Benan";
+			// Burada konsol ekranına yeni değeri yazar.
+			// Console.WriteLine("Yeni Değer: " + customerName);
+			customerName = "Benan";
+			customerSurname = "Baz";
+			customerPhone = "+905445085479";
+			customerEmail = "benan456@gmail.com";
+			customerDistrict = "Çukurova";
+			customerCity = "Adana";
+
+			//Console.WriteLine("------------------------------------------------------");
+			// String Toplama: Birden fazla string değeri "+" yani toplama operatörü ile toplayıp yan yana yazılmasını sağlayabiliriz.
+			//Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+			// "\n" string ifadelerde satır atlama için kullanılır.
+			//Console.WriteLine("İletişim: " + customerPhone + "\n" + "Email: " + customerEmail);
+			//Console.WriteLine("Adres: " + customerCity + "/" + customerCity);
+			//Console.WriteLine("------------------------------------------------------");
+			#endregion
+
+			#region Integer Değişkenler
+			// int: Tam sayı türündeki değişkenlerdir.
+			//int age = 20;
+			//Console.WriteLine("Kullanıcının yaşı: " + age);
+			int hamburgerPrice = 300;
+			int cokePrice = 35;
+			int waterPrice = 10;
+			int friesPrice = 50;
+			int pizzaPrice = 250;
+			int lemonadePrice = 30;
+
+            Console.WriteLine("**** Restoran Menü Fiyatı ****");
+            Console.WriteLine();
+
+            Console.WriteLine("-> Hamburger: " + hamburgerPrice + " TL");
+            Console.WriteLine("-> Kola: " + cokePrice + " TL");
+            Console.WriteLine("-> Su: " + waterPrice + " TL");
+            Console.WriteLine("-> Kızartma Çeşitleri: " + friesPrice + " TL");
+            Console.WriteLine("-> Pizza: " + pizzaPrice + " TL");
+            Console.WriteLine("-> Limonata: " + lemonadePrice + " TL");
+
+			Console.WriteLine();
+
+
+			int hamburgerCount;
+			int cokeCount;
+			int waterCount;
+			int friesCount;
+			int pizzaCount;
+			int lemonadeCount;
+
+			int totalPrice = 0;
+
+			hamburgerCount = 3;
+			cokeCount = 3;
+			waterCount = 3;
+			friesCount = 1;
+			pizzaCount = 0;
+			lemonadeCount = 0;
+
+			totalPrice = ((hamburgerCount * hamburgerPrice) + (cokeCount * cokePrice) + (waterCount * waterPrice) + (friesCount * friesPrice) + (pizzaCount * pizzaPrice) + (lemonadeCount * lemonadePrice));
+            
+			Console.WriteLine();
+			Console.WriteLine("---- Adisyon ----");
+            Console.WriteLine();
+			Console.WriteLine("-> Hamburger: " + hamburgerPrice + " TL * " +  hamburgerCount + " Adet" + " -> " + (hamburgerCount * hamburgerPrice) + " TL");
+            Console.WriteLine("-> Kola: " + cokePrice + " TL * " +  cokeCount + " Adet" + " -> " + (cokeCount * cokePrice) + " TL");
+            Console.WriteLine("-> Su: " + waterPrice + " TL * " +  waterCount + " Adet" + " -> " + (waterCount * waterPrice) + " TL");
+            Console.WriteLine("-> Kızartma: " + friesPrice + " TL * " +  friesCount + " Adet" + " -> " + (friesCount * friesPrice) + " TL");
+            Console.WriteLine();
+			Console.WriteLine("----> Toplam Ödenecek Tutar: " + totalPrice + " TL");
+            Console.WriteLine();
+            Console.WriteLine("******************************");
+
+			// Ders Tamamlandı!
+			#endregion
+
+		}
+	}
+}
---
+++ 02-Variables/Program.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello World!");
+
+			//Variables - Değişkenler
+
+			// string ve int değişken türlerini görmüştük, şimdi diğer değişken türleri ile devam edelim;
+
+			#region Double Değişkenler
+			//double değişkenler
+			///*
+			//  Ondalıklı sayılar için kullanılır.
+			//*/
+			//double length = 1.67;
+			//Not->Kod tarafında ondalıklı sayıyı nokta ile tanımlarken,
+			//program tarafında virgül ile gözüktü. Buradan şu sonucu çıkarabiliriz;
+			//Kod tarafında ondalıklı ifadeyi belirttiğimiz nokta sembolü, Konsol ekranında bize virgül olarak dönmektedir.
+			//Console.WriteLine($"Kullanıcının boyu: {length}");
+
+			//Pratik yapalım;
+			// > Meyve ve Sebzeler kilogramı ile beraber oluşturulsun,
+			// > Oluşturulan meyve ve sebzelere kilogram fiyatları belirlensin,
+			// > Çıkan sonuç ekrana yazdırılsın.
+
+			// 1 - Meyve ve Sebzeleri Oluşturalım;
+
+			//string apple = "Kırmızı Elma";
+			//string watermelon = "Karpuz";
+			//string banana = "Muz";
+			//string pear = "Armut";
+
+			//string pumpkin = "Kabak";
+			//string beans = "Fasulye";
+			//string eggplant = "Patlıcan";
+			//string pepper = "Biber";
+
+			//2 - Oluşturduğumuz Meyve ve Sebzelerin Kilogramlarını Belirleyelim;
+
+			//double appleWeight = 1.89;
+			//double watermelonWeight = 8.79;
+			//double bananaWeight = 2.89;
+			//double pearWeight = 5.78;
+
+			//double pumpkinWeight = 15.78;
+			//double beansWeight = 6.42;
+			//double eggplantWeight = 4.36;
+			//double pepperWeight = 9.23;
+
+			//3 - Kilogram Birimini Tanımlayalım;
+
+			//string unit = "Kilogram";
+			//string shortUnit = "Kg";
+
+			//4 - Oluşturduğumuz Meyve Ve Sebzelerin Birim Başına Fiyatlarını Belirleyelim;
+
+			//double applePricePerUnit = 5.50;
+			//double watermelonPricePerUnit = 9.90;
+			//double bananaPricePerUnit = 8.20;
+			//double pearPricePerUnit = 2.36;
+
+			//double pumpkinPricePerUnit = 2.10;
+			//double beansPricePerUnit = 1.50;
+			//double eggplantPricePerUnit = 1;
+			//double pepperPricePerUnit = 0.50;
+
+			//Para birimimizi belirleyelim;
+			//string priceUnit = "TL";
+
+			//5 - Oluşturduğumuz Meyve ve Sebzelerin Satın Alınan Kilogram ile Birim Fiyatını Çarparak Ödenmesi Gereken Ücreti Yazalım;
+
+			//Console.WriteLine();
+			//Console.WriteLine("****** BAZ MANAV ******");
+			//Console.WriteLine();
+
+			//Console.WriteLine("*** Ürünler ***");
+			//Console.WriteLine("-----------------");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {apple} {shortUnit} -> {applePricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {watermelon} {shortUnit} -> {watermelonPricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {banana} {shortUnit} -> {bananaPricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {pear} {shortUnit} -> {pearPricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {pumpkin} {shortUnit} -> {pumpkinPricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {beans} {shortUnit} -> {beansPricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {eggplant} {shortUnit} -> {eggplantPricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine($"> {pepper} {shortUnit} -> {pepperPricePerUnit}{priceUnit}");
+			//Console.WriteLine();
+			//Console.WriteLine("-------------------------------------");
+			//Console.WriteLine();
+
+			//Müşteri her üründen 1.5 kg alım yapsın
+			//double customerShopWeight = 1.5;
+
+			//Console.WriteLine("*** Adisyon ***");
+			//Console.WriteLine("-----------------");
+			//Console.WriteLine($"> {apple} {shortUnit} -> {applePricePerUnit * customerShopWeight}{priceUnit}");
+			//Console.WriteLine($"> {watermelon} {shortUnit} -> {watermelonPricePerUnit * customerShopWeight}{priceUnit}");
+			//Console.WriteLine($"> {banana} {shortUnit} -> {bananaPricePerUnit * customerShopWeight}{priceUnit}");
+			//Console.WriteLine($"> {pear} {shortUnit} -> {pearPricePerUnit * customerShopWeight}{priceUnit}");
+			//Console.WriteLine($"> {pumpkin} {shortUnit} -> {pumpkinPricePerUnit * customerShopWeight}{priceUnit}");
+			//Console.WriteLine($"> {beans} {shortUnit} -> {beansPricePerUnit * customerShopWeight}{priceUnit}");
+			//Console.WriteLine($"> {eggplant} {shortUnit} -> {eggplantPricePerUnit * customerShopWeight}{priceUnit}");
+			//Console.WriteLine($"> {pepper} {shortUnit} -> {pepperPricePerUnit * customerShopWeight}{priceUnit}");
+
+			//double totalPrice = ((applePricePerUnit * customerShopWeight) + (watermelonPricePerUnit * customerShopWeight) + (bananaPricePerUnit * customerShopWeight) + (pearPricePerUnit * customerShopWeight) + (pumpkinPricePerUnit * customerShopWeight) + (beansPricePerUnit * customerShopWeight) + (eggplantPricePerUnit * customerShopWeight) + (pepperPricePerUnit * customerShopWeight));
+			//Console.WriteLine("-------------------------------------");
+			//Console.WriteLine(); Console.WriteLine($"Toplam Ödenmesi Gereken: {totalPrice}");
+
+			#endregion
+
+			#region Char Değişkenler
+			// Karakter değeri tutabilen değişken türüdür.
+			// Tek tırnak içerisinde değer tutulmalıdır, aksi halde hata verecektir.
+			char userChar = 'G';
+			//Console.WriteLine($"Kullanıcı Karakteri: {userChar}");
+			#endregion
+
+			#region Klavyeden Veri Girişleri - String Türünde Veri Alma
+			//Console.WriteLine("**** BAZ Hava Yolları Yolcu Bilgisi ****");
+			//Console.WriteLine();
+
+			//string customerName, customerSurname, customerDistrict, customerCity, customerAge, customerIdentityNumber;
+
+			//Console.Write("Adınızı Giriniz: ");
+			// Console.ReadLine() : Konsol satırını okumaya yaramaktadır.
+			// Biz öncesinde konsol ekranını kapatmasın diye kullanıyorduk çünkü konsol ekranını okuduğu için konsolun kapanmasını engellemiş oluyor.
+			// Konsoldan string veri alabilmek için ReadLine kullanıyoruz.
+			// Aslında çalışma mantığı aynı metin giriş kutucukları gibidir içerisine veriyi girer ve bir değişkene atarız, ilgili değişken girilmiş veriyi tutar. Böylece dışarıdan kullanıcı programa veri girişi yapabilir.
+			// WriteLine satıra yazıyor, ReadLine satırı okuyor.
+			//customerName = Console.ReadLine();
+
+			//Console.Write("Soyadınızı Giriniz: ");
+			//customerSurname = Console.ReadLine();
+
+			//Console.Write("İlçenizi Giriniz: ");
+			//customerDistrict = Console.ReadLine();
+
+			//Console.Write("İlinizi Giriniz: ");
+			//customerCity = Console.ReadLine();
+
+			//Console.Write("Yaşınızı Giriniz: ");
+			//customerAge = Console.ReadLine();
+
+			//Console.Write("TC Kimlik Numaranızı Giriniz: ");
+			//customerIdentityNumber = Console.ReadLine();
+
+			//Console.WriteLine("------------------------------------------");
+			//Console.WriteLine($"Yolcu Adı: {customerName}");
+			//Console.WriteLine($"Yolcu Soyadı: {customerSurname}");
+			//Console.WriteLine($"Yolcu İlçesi: {customerDistrict}");
+			//Console.WriteLine($"Yolcu İli: {customerCity}");
+			//Console.WriteLine($"Yolcu Yaşı: {customerAge}");
+			//Console.WriteLine($"Yolcu Kimlik Numarası: {customerIdentityNumber}");
+			//Console.WriteLine("------------------------------------------");
+
+
+			#endregion
+
+			#region Klavyeden Veri Girişleri - Int Türünde Veri Alma ve Dönüşümler
+
+			// C# programlama dili konsol ekranına girilen değerleri başlangıçta string olarak kabul etmektedir.
+			// Eğer biz başka türde değer girip işlem yapacaksak tip dönüştürme işlemi yani tip dönüşümü yapmamız gerekmektedir.
+			// Program aslında bana girilen değerin sayı olduğunu bildir demek istiyor.
+
+			// string değeri en kolay biçimde int değere dönüştürmek için: int.Parse(donusturulecekDeger) kullanabiliriz.
+
+			//int laptopPrice = 25000;
+			//int desktopPrice = 30000;
+			//int monitorPrice = 6500;
+			//int mousePrice = 3000;
+			//int keyboardPrice = 4000;
+
+			//         Console.Write("Satın almak istediğiniz laptop miktarını giriniz: ");
+			//int laptopCount = int.Parse(Console.ReadLine());
+
+			//Console.Write("Satın almak istediğiniz masaüstü miktarını giriniz: ");
+			//int desktopCount = int.Parse(Console.ReadLine());
+
+			//Console.Write("Satın almak istediğiniz monitör miktarını giriniz: ");
+			//int monitorCount = int.Parse(Console.ReadLine());
+
+			//Console.Write("Satın almak istediğiniz fare miktarını giriniz: ");
+			//int mouseCount = int.Parse(Console.ReadLine());
+
+			//Console.Write("Satın almak istediğiniz klavye miktarını giriniz: ");
+			//int keyboardCount = int.Parse(Console.ReadLine());
+
+			//// Klavyeden girilen değeri int türüne parse et, dönüştür demiş olduk.
+
+			//int totalPrice = (laptopCount * laptopPrice) + (desktopCount * desktopPrice) + (monitorCount * monitorPrice) + (mouseCount * mousePrice) + (keyboardCount * keyboardPrice);
+
+			//         Console.WriteLine();
+
+			//         Console.WriteLine($"Toplam ödemeniz gereken miktar: {totalPrice} TL");
+
+
+			#endregion
+
+			#region Klavyeden Veri Girişleri - Double Türünde Veri Alma ve Dönüşümler
+			//double exam1, exam2, exam3, result;
+			//Console.Write("Lütfen birinci sınav notunu giriniz: ");
+			//int'da olduğu gibi double türünde de Parse metotunu kullanarak dönüşüm işlemlerini yapabilmekteyiz.
+			//exam1 = double.Parse(Console.ReadLine());
+
+			//Console.Write("Lütfen ikinci sınav notunu giriniz: ");
+			//exam2 = double.Parse(Console.ReadLine());
+
+			//Console.Write("Lütfen üçüncü sınav notunu giriniz: ");
+			//exam3 = double.Parse(Console.ReadLine());
+
+			//result = (exam1 + exam2 + exam3) / 3;
+
+			//Console.WriteLine();
+
+			//Console.WriteLine($"Ortalamanız: {result}");
+			#endregion
+
+			#region Klavyeden Veri Girişleri - Char Türünde Veri Alma ve Dönüşümleri
+			//char gender;
+			//Console.Write("Lütfen cinsiyet seçiniz: ");
+			//gender = char.Parse(Console.ReadLine());
+
+			//Console.WriteLine();
+
+			//Console.WriteLine($"Seçilen Cinsiyet: {gender}");
+			#endregion
+
+			// Ders Tamamlandı!
+
+			Console.ReadLine();
+
+		}
+	}
+}
